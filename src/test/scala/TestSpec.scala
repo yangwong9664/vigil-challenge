5d9df75d5e5d1814849578f3bc3bb378c45be281
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2023-08-21 18:07:19 +0100

    fix tests

--- src/test/scala/TestSpec.scala
+++ src/test/scala/TestSpec.scala
@@ -1470,7 +1470,6 @@ class TestSpec extends AnyWordSpec with Matchers {
       val result = formatter.format(beeMovieScript, charLimit)
 
       forAll(result) { line =>
-        println(line)
         assert(line.length <= charLimit)
       }
 
